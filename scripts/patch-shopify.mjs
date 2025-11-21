86e880f2f680d77f026efc57c56aa53dc6bfd80c
ravijain7154 <ravijain7154@gmail.com>
2025-11-21 08:01:52 +0530

    Improve code 21-11-2025

--- scripts/patch-shopify.mjs
+++ scripts/patch-shopify.mjs
@@ -31,7 +31,7 @@ const fs = require("fs");
 const path = require("path");
 
 const file = path.resolve(
-  "node_modules/@shopify/shopify-app-remix/dist/cjs/react/components/AppProvider/AppProvider.js"
+  "node_modules/@shopify/shopify-app-remix/dist/esm/react/components/AppProvider/AppProvider.mjs"
 );
 
 let code = fs.readFileSync(file, "utf8");
