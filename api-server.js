931e0b36fc8059918abaf4bf3b54d2c00a360fe5
ravijain7154 <ravijain7154@gmail.com>
2026-02-15 17:17:59 +0530

    change complete workin with  proxy and exrtention 15 frb 2026

--- api-server.js
+++ api-server.js
@@ -132,7 +132,7 @@ const staticCorsMiddleware = (req, res, next) => {
 
 // Static files for diamond filter UI (CORS applied)
 // app.use('/diamond-filter', express.static(path.join(process.cwd(), 'public')));
-app.get('apps/diamond-filter', (req, res) => {
+app.get('/apps/diamond-filter', (req, res) => {
   res.set('Content-Type', 'text/html');
 
   res.send(`
@@ -176,7 +176,7 @@ app.use('/diamond-filter/assets', express.static(path.join(process.cwd(), 'publi
 
 
 // Endpoint to fetch products from the database
-app.get('/api/products', async(req, res) => {
+app.get('/apps/diamond-filter/api/products', async(req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
         const perPage = parseInt(req.query.perPage) || 25;
@@ -450,7 +450,7 @@ app.get('/api/products', async(req, res) => {
     }
 });
 
-app.get('/api/diamond-detail', async(req, res) => {
+app.get('/apps/diamond-filter/api/diamond-detail', async(req, res) => {
     try {
         const Stock_id = req.query.Stock_id;
                 // Add shape filter if present
@@ -477,7 +477,7 @@ app.get('/api/diamond-detail', async(req, res) => {
 
 
 // Endpoint to save color to style.css
-app.get('/api/get-color', async (req, res) => {
+app.get('/apps/diamond-filter/api/get-color', async (req, res) => {
     try {
         const colorSetting = await prisma.colorsetting.findFirst({
             orderBy: { createdAt: 'desc' },  // Get the most recent color setting
