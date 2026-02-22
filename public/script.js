fe647b13c52ed4f5cfe738015b3e074bee89f1fa
ravijain7154 <ravijain7154@gmail.com>
2026-02-22 20:14:58 +0530

    change in api-server.js for price and carat 22-02-2026

--- public/script.js
+++ public/script.js
@@ -45,7 +45,7 @@ function initMoreFilterToggle() {
   // 1️⃣ Load CSS explicitly
   const styles = [
     `${APP_URL}/diamond-filter/assets/css/styles.css`,
-    `${APP_URL}/diamond-filter/assets/css/datatables.min.css`,
+    //`${APP_URL}/diamond-filter/assets/css/datatables.min.css`,
     `${APP_URL}/diamond-filter/assets/css/ring_builder.css`,
     `${APP_URL}/diamond-filter/assets/css/diamond.css`,
     `${APP_URL}/diamond-filter/assets/css/ion.rangeSlider.css`,
