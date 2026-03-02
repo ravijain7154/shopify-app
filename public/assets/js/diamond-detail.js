802c24d72e6607a81c81fdf6d75fa17baf2a0163
ravijain7154 <ravijain7154@gmail.com>
2026-03-03 01:34:41 +0530

    change in diamond details 03032025

--- public/assets/js/diamond-detail.js
+++ public/assets/js/diamond-detail.js
@@ -173,11 +173,11 @@ function renderThumb(item, isActive) {
     `;
   }
 
-  const icon = item.type === "video" ? "▶" : "📄";
+  const icon = item.type === "video" ? "https://shopify-app-pndl.onrender.com/diamond-filter/assets/images/vdobig.png" : "https://shopify-app-pndl.onrender.com/diamond-filter/assets/images/notebook.svg";
   return `
     <button type="button" class="media-thumb media-thumb-icon${activeClass}" data-media-type="${item.type}" data-media-url="${item.url}">
       <span aria-hidden="true">${icon}</span>
-      <span>${item.label}</span>
+      // <span>${item.label}</span>
     </button>
   `;
 }
